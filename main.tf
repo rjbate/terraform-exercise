9b66f75c520ca742e88ed74e090a86e8824d4b73
andresb39 <andresb39@gmail.com>
2020-12-01 08:49:01 -0500

    Add: ELB config

--- main.tf
+++ main.tf
@@ -13,7 +13,17 @@ resource "aws_subnet" "public-subnet" {
   availability_zone = "${var.region}a"
 }
 
+resource "aws_subnet" "public-subnet2" {
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = "${var.subnet-cidr-public2}"
+  availability_zone = "${var.region}b"
+}
+
 resource "aws_route_table" "public-subnet-route-table" {
+  vpc_id = "${aws_vpc.vpc.id}"
+}
+
+resource "aws_route_table" "public-subnet-route-table2" {
   vpc_id = "${aws_vpc.vpc.id}"
 }
 
@@ -27,28 +37,55 @@ resource "aws_route" "public-subnet-route" {
   route_table_id          = "${aws_route_table.public-subnet-route-table.id}"
 }
 
+resource "aws_route" "public-subnet-route2" {
+  destination_cidr_block  = "0.0.0.0/0"
+  gateway_id              = "${aws_internet_gateway.igw.id}"
+  route_table_id          = "${aws_route_table.public-subnet-route-table2.id}"
+}
+
 resource "aws_route_table_association" "public-subnet-route-table-association" {
   subnet_id      = "${aws_subnet.public-subnet.id}"
   route_table_id = "${aws_route_table.public-subnet-route-table.id}"
+}
+
+resource "aws_route_table_association" "public-subnet-route-table-association2" {
+  subnet_id      = "${aws_subnet.public-subnet2.id}"
+  route_table_id = "${aws_route_table.public-subnet-route-table2.id}"
 }
 
 resource "aws_key_pair" "web" {
   public_key = "${file(pathexpand(var.public_key))}"
 }
 
+
 resource "aws_instance" "web-instance" {
-  ami           = "ami-cdbfa4ab"
-  instance_type = "t2.small"
+  ami           = "ami-09558250a3419e7d0"
+  instance_type = "t2.micro"
   vpc_security_group_ids      = [ "${aws_security_group.web-instance-security-group.id}" ]
   subnet_id                   = "${aws_subnet.public-subnet.id}"
   associate_public_ip_address = true
   key_name                    = "${aws_key_pair.web.key_name}"
   user_data                   = <<EOF
 #!/bin/sh
-yum install -y nginx
-service nginx start
+sudo amazon-linux-extras install nginx1 -y
+sudo service nginx start
 EOF
 }
+
+resource "aws_instance" "web-instance2" {
+  ami           = "ami-09558250a3419e7d0"
+  instance_type = "t2.micro"
+  vpc_security_group_ids      = [ "${aws_security_group.web-instance-security-group.id}" ]
+  subnet_id                   = "${aws_subnet.public-subnet2.id}"
+  associate_public_ip_address = true
+  key_name                    = "${aws_key_pair.web.key_name}"
+  user_data                   = <<EOF
+    #!/bin/sh
+    sudo amazon-linux-extras install nginx1 -y
+    sudo service nginx start
+    EOF
+}
+
 
 resource "aws_security_group" "web-instance-security-group" {
   vpc_id      = "${aws_vpc.vpc.id}"
@@ -76,6 +113,48 @@ resource "aws_security_group" "web-instance-security-group" {
   }
 }
 
-output "web_domain" {
-  value = "${aws_instance.web-instance.public_dns}"
+
+## Security Group for ELB
+resource "aws_security_group" "elb" {
+  name = "elb"
+  vpc_id = "${aws_vpc.vpc.id}"
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+### Creating ELB
+resource "aws_elb" "web_elb" {
+  name = "web-elb1"
+  security_groups = ["${aws_security_group.elb.id}"]
+  instances = ["${aws_instance.web-instance.id}", "${aws_instance.web-instance2.id}"]
+  subnets = [ "${aws_subnet.public-subnet.id}", "${aws_subnet.public-subnet2.id}" ]
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:80/"
+  }
+
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "80"
+    instance_protocol = "http"
+  }
+}
+
+output "web_domain" {
+  value = ["${aws_instance.web-instance.public_dns}", "${aws_instance.web-instance2.public_dns}", "${aws_elb.web_elb.dns_name}" ]
+}
+
--- variables.tf
+++ variables.tf
@@ -4,6 +4,8 @@ variable "vpc-cidr" {}
 
 variable "subnet-cidr-public" {}
 
+variable "subnet-cidr-public2" {}
+
 # for the purpose of this exercise use the default key pair on your local system
 variable "public_key" {
   default = "~/.ssh/id_rsa.pub"
